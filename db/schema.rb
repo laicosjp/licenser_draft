9055659a58c5d0ee02a76ae97a1ea7a7217f0c71
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-23 17:48:32 +0900

    feat: カテゴリーに英名カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_043929) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "en_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
